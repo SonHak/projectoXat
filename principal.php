92d6c86dea17fc02dea4fccbf9bda899c8af66bb
Adrian Cardenas <SonHak@github.com>
2018-01-18 18:55:55 +0100

    includes

--- principal.php
+++ principal.php
@@ -36,13 +36,13 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#about">Chat</a>
+              <a class="nav-link js-scroll-trigger" href="#about">Foro</a>
             </li>
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#download">Denuncias</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#contact">Foro</a>
+              <a class="nav-link js-scroll-trigger" href="#contact">Chat</a>
             </li>
           </ul>
         </div>
@@ -56,8 +56,6 @@
           <div class="row">
             <div class="col-lg-8 mx-auto">
               <h1 class="brand-heading">PROYECTO VOTA</h1>
-              <p class="intro-text">A free, responsive, one page Bootstrap theme.
-                <br>Created by Start Bootstrap.</p>
               <a href="#about" class="btn btn-circle js-scroll-trigger">
                 <i class="fa fa-angle-double-down animated"></i>
               </a>
@@ -72,14 +70,10 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-8 mx-auto">
-            <h2>About Grayscale</h2>
-            <p>Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
-              <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
-            <p>This theme features stock photos by
-              <a href="http://gratisography.com/">Gratisography</a>
-              along with a custom Google Maps skin courtesy of
-              <a href="http://snazzymaps.com/">Snazzy Maps</a>.</p>
-            <p>Grayscale includes full HTML, CSS, and custom JavaScript files along with SASS and LESS files for easy customization!</p>
+            <!--AQUÍ VA EL FORO-->
+            <?php
+            include('foro.php');
+            ?>
           </div>
         </div>
       </div>
@@ -89,12 +83,22 @@
     <section id="download" class="download-section content-section text-center">
       <div class="container">
         <div class="col-lg-8 mx-auto">
-          <h2>Download Grayscale</h2>
-          <p>You can download Grayscale for free on the preview page at Start Bootstrap.</p>
-          <a href="http://startbootstrap.com/template-overviews/grayscale/" class="btn btn-default btn-lg">Visit Download Page</a>
+          <!--AQUÍ VA DEBATES-->
+          <?php
+            include('denuncia.php');
+          ?>
         </div>
       </div>
     </section>
+
+
+    <!-- AQUÍ VA EL CHAT-->
+    <!-- Map Section -->
+    <div id="chat">
+      <?php 
+        include('chat.php');
+      ?>
+    </div>
 
     <!-- Contact Section -->
     <section id="contact" class="content-section text-center">
@@ -129,9 +133,6 @@
         </div>
       </div>
     </section>
-
-    <!-- Map Section -->
-    <div id="map"></div>
 
     <!-- Footer -->
     <footer>
