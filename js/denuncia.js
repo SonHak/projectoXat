257638787e3c7193f983d32e1049b83a97c1cec6
Adrian Cardenas <SonHak@github.com>
2018-01-25 20:26:09 +0100

    form

--- js/denuncia.js
+++ js/denuncia.js
@@ -1,7 +1,30 @@
+$(function(){
+	$('#DBotonNuevo').click(function(){
+		$('#denunciaForm').delay(800).fadeIn(FadeOutTab('#myCarousel'));
+	})
+	
+	$('#DBotonVolver').click(function(){
+		$('#myCarousel').delay(800).fadeIn(FadeOutTab('#denunciaForm'));
+	})
+})
+
+
+
+
+
+function FadeInTab(elemento){
+	$(elemento).fadeIn(1000);
+}
+
+function FadeOutTab(elemento){
+	$(elemento).fadeOut(1000);
+}
+
+
 function myMap() {
   var mapCanvas = document.getElementById("map");
   var mapOptions = {
-    center: new google.maps.LatLng(51.5, -0.2), zoom: 10
+    center: new google.maps.LatLng(41.4, 2.1), zoom: 10
   };
   var map = new google.maps.Map(mapCanvas, mapOptions);
 }
